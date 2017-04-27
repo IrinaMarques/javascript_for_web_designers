3d7af2695fdeeeab78c5122723de94f9a248dd29
IrinaMarques <irinavpmarques@gmail.com>
2017-04-27 18:01:30 +0100

    2.2 Data Types The 5 primary data types: string, number, boolean, null and undefined. The typeof operator

---
+++ files/2.2script.js
@@ -0,0 +1,6 @@
+// Primary
+var name = "John", // string - can use double or single quotes
+	age = 32, // number - without quotes
+	married = true, //boolean - true or false
+	age = null, // null - erase the contents of a variable without deleting the variable itself
+	carModel;
