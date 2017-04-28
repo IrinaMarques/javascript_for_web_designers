0659325efffd179c1a0c97f6af9c6ae601dbac87
IrinaMarques <irinavpmarques@gmail.com>
2017-04-28 19:18:56 +0100

    2.3 Arrays Properties - length propertie Methods - push, pop, shift, unshift, indexOf

---
+++ files/2.3script.js
@@ -0,0 +1,7 @@
+// Array
+
+// Ways of declare an array
+// variable list equals square brakets and the list of values
+var list = ["John", "Marie", "Sander", 32, false],
+	list2 = [],
+	list3 = new Array(12, false);
