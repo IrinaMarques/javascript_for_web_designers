6da7ebd3971e68486df5a128195d7acf6b9c8f95
IrinaMarques <irinavpmarques@gmail.com>
2017-05-02 17:15:28 +0100

    2.4 Objects

---
+++ files/2.4script.js
@@ -0,0 +1,25 @@
+// Objects
+
+var person = new Object(),
+	// or:
+
+//var person = {
+	//name: "Adi",
+	//age: 30;
+//}
+
+// add properties to these objects
+
+person.name = "James";
+person.age = 43;
+person.height = 1.76;
+person.married = true;
+
+person["name"] = "Kirk";
+
+person.car = {};
+
+person.car.make = "Honda";
+
+
+
