a94b5e35e26134f62aa9dc72bf54ed9904498b03
IrinaMarques <irinavpmarques@gmail.com>
2017-05-02 16:49:26 +0100

    Added Extra content for understanding

--- files/2.2script.js
+++ files/2.2script.js
@@ -3,4 +3,6 @@ var name = "John", // string - can use double or single quotes
 	age = 32, // number - without quotes
 	married = true, //boolean - true or false
 	age = null, // null - erase the contents of a variable without deleting the variable itself
-	carModel;
+	carModel; // unedifined
+
+
--- files/2.3script.js
+++ files/2.3script.js
@@ -5,3 +5,16 @@
 var list = ["John", "Marie", "Sander", 32, false],
 	list2 = [],
 	list3 = new Array(12, false);
+
+
+var array = [];
+
+var keep = function() {
+	array.push( document.getElementById('text').value )
+	console.log(array)
+}
+
+var remove = function() {
+	array.pop()
+	console.log(array)
+}
