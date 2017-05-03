aef393d738d4eaa06c7b885685a61035e11451a2
IrinaMarques <irinavpmarques@gmail.com>
2017-05-03 12:53:43 +0100

    2.5 Operators
    Assigment operator - equal, addition, subtraction, multiplication, division
    Comparision and logical operators
    Arithmetic operators - increment, decrement
    Concatenation operator

---
+++ files/2.5script.js
@@ -0,0 +1,24 @@
+// Operators:
+
+var x = 1;
+var y = 3;
+
+//Equals assignment
+x = y;
+
+//Addition assignment
+x = x + y;
+x += y;
+
+//Subtraction assignment
+x -=y;
+x = x - y;
+
+//Multiplication assignment
+x *= y;
+x = x * y;
+
+//Division assignment
+x /= y;
+x = x / y;
+
